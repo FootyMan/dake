4c34844f61d0f1ae611ba7f558e5ef6cb26cc78c
haochengyang <haochengyang@izuche.com>
2018-07-23 13:32:14 +0800

    1

---
+++ src/main/java/com/unicom/requestresponse/WebContextFilter.java
@@ -0,0 +1,49 @@
+package com.unicom.requestresponse;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+
+public class WebContextFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse rep, FilterChain chain)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) rep;
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers", "Content-Type,Accept, X-Requested-With,Authorization");
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+//		if (request.getMethod().equals("OPTIONS"))
+//		{
+//			response.setStatus(200);
+//
+//		}
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
